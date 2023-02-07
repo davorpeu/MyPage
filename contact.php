5ce98668019828258041fec1e75adb882878074e
dadop <davorpeu@gmail.com~>
2023-02-07 14:22:14 +0100

    Implemented table to display all messages stored

--- contact.php
+++ contact.php
@@ -18,6 +18,50 @@
     <?php
     if (isset($_GET["success"]) && $_GET["success"] == 1) {
       echo '<div style="background-color: red; color:white;"><h1>Prošlo je</h1></div>';
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "email";
+    
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+    
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+    
+        // Prepare SQL SELECT statement
+        $sql = "SELECT name, email, message, date FROM email";
+    
+        // Execute SELECT statement
+        $result = mysqli_query($conn, $sql);
+    
+        if (mysqli_num_rows($result) > 0) {
+            echo "<table>";
+            echo "<tr>";
+            echo "<th>Name</th>";
+            echo "<th>Email</th>";
+            echo "<th>Message</th>";
+            echo "<th>Date</th>";
+            echo "</tr>";
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row["name"] . "</td>";
+                echo "<td>" . $row["email"] . "</td>";
+                echo "<td>" . $row["message"] . "</td>";
+                echo "<td>" . $row["date"] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "No data found.";
+        }
+    
+        // Close connection
+        mysqli_close($conn);
+    
     }
     ?>
     <h1 class="title">Kontaktirajte me</h1>
